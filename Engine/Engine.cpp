09cddf7a2119a72fa2f7ab2d0a89c6fae7715892
Avvakira <OlgaCreativee@gmail.com>
2025-07-17 17:32:57 +0300

    дописала остальные методы, исправила engine и добавила игрока

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,12 +1,53 @@
 #include "pch.h"
 #include "Engine.h"
 #include <iostream>
-Engine::Engine() {}
-void Engine::Initialize()
+#include "GameWorld.h"
+#include "RenderSystem.h"
+
+namespace Engine
 {
-	std::cout << "Engine Init" << std::endl;
-}
-void Engine::Run()
-{
-	std::cout << "Engine runs!" << std::endl;
+	Engine* Engine::Instance()
+	{
+		static Engine instance;
+		return &instance;
+	}
+
+	Engine::Engine()
+	{
+		unsigned int seed = (unsigned int)time(nullptr);
+		srand(seed);
+	}
+
+	void Engine::Run()
+	{
+		sf::Clock gameClock;
+		sf::Event event;
+
+		while (RenderSystem::Instance()->GetMainWindow().isOpen())
+		{
+			sf::Time dt = gameClock.restart();
+			float deltaTime = dt.asSeconds();
+
+			while (RenderSystem::Instance()->GetMainWindow().pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+				{
+					RenderSystem::Instance()->GetMainWindow().close();
+				}
+			}
+
+			if (!RenderSystem::Instance()->GetMainWindow().isOpen())
+			{
+				break;
+			}
+
+			RenderSystem::Instance()->GetMainWindow().clear();
+
+			GameWorld::Instance()->Update(deltaTime);
+			GameWorld::Instance()->Render();
+			GameWorld::Instance()->LateUpdate();
+
+			RenderSystem::Instance()->GetMainWindow().display();
+		}
+	}
 }
