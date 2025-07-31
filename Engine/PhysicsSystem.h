a994a770c206cb32ba631db4c5c8d595a25606b8
Avvakira <OlgaCreativee@gmail.com>
2025-07-31 18:49:18 +0300

    добавлены другие методы физики

--- Engine/PhysicsSystem.h
+++ Engine/PhysicsSystem.h
@@ -1,9 +1,30 @@
 #pragma once
+#include <iostream>
+#include "ColliderComponent.h"
+#include "RigidbodyComponent.h"
+#include "Vector.h"
 
 namespace GameEngine
 {
     class PhysicsSystem {
     public:
         static PhysicsSystem* Instance();
+
+        void Update();
+
+        float GetFixedDeltaTime() const;
+        void Subscribe(ColliderComponent* collider);
+        void Unsubscribe(ColliderComponent* collider);
+    private:
+        PhysicsSystem() {}
+        ~PhysicsSystem() {}
+
+        PhysicsSystem(PhysicsSystem const&) = delete;
+        PhysicsSystem& operator= (PhysicsSystem const&) = delete;
+
+        std::vector<ColliderComponent*> colliders;
+        std::map<ColliderComponent*, ColliderComponent*> triggersEnteredPair;
+
+        float fixedDeltaTime = 0.02f;
     };
 }
--- Engine/Trigger.h
+++ Engine/Trigger.h
@@ -1,1 +1,17 @@
 #pragma once
+
+namespace GameEngine
+{
+	class ColliderComponent;
+	struct Trigger
+	{
+	public:
+		Trigger(ColliderComponent* newFirst, ColliderComponent* newSecond) :
+			first(newFirst), second(newSecond)
+		{
+		};
+	private:
+		ColliderComponent* first;
+		ColliderComponent* second;
+	};
+}
